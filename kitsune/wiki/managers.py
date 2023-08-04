0e210af82a46251060c3a456c8e8f83212a75ac6
Ryan Johnson <rjohnson@mozilla.com>
2023-08-04 12:20:45 -0700

    add ability to restrict visibility of documents

--- kitsune/wiki/managers.py
+++ kitsune/wiki/managers.py
@@ -12,6 +12,10 @@ class VisibilityManager(models.Manager):
     # should be "document".
     document_relation: str | None = None
 
+    @property
+    def document_relation_prefix(self):
+        return f"{self.document_relation}__" if self.document_relation else ""
+
     def get_creator_condition(self, user):
         """
         Return a conditional (e.g., a Q or Exists object) that is only true
@@ -19,17 +23,55 @@ class VisibilityManager(models.Manager):
         """
         raise NotImplementedError
 
+    def unrestricted(self, user, **kwargs):
+        """
+        Documents are unrestricted for a given user if they're not restricted
+        to staff or a specific group, or when the user is staff/superuser and
+        they're restricted by staff, or when the user is a member of the specific
+        group to which they're restricted. A translation (i.e., a document with
+        a parent), follows its parent's restrictions.
+        """
+        prefix = self.document_relation_prefix
+
+        def unrestricted_condition(parent_prefix=""):
+            """
+            Create the unrestricted condition for either the document or
+            the document's parent.
+            """
+            restrict_to_staff = f"{prefix}{parent_prefix}restrict_to_staff"
+            restrict_to_group = f"{prefix}{parent_prefix}restrict_to_group"
+
+            parent_condition = Q(**{f"{prefix}parent__isnull": not bool(parent_prefix)})
+            not_restricted = Q(**{restrict_to_staff: False}) & Q(
+                **{f"{restrict_to_group}__isnull": True}
+            )
+            staff_condition_fulfilled = Q(**{restrict_to_staff: True}) & Q(
+                **{restrict_to_staff: user.is_superuser or user.is_staff}
+            )
+            group_condition_fulfilled = Q(**{f"{restrict_to_group}__isnull": False}) & Q(
+                **{f"{restrict_to_group}__in": list(user.groups.values_list("id", flat=True))}
+            )
+
+            return parent_condition & (
+                not_restricted | staff_condition_fulfilled | group_condition_fulfilled
+            )
+
+        return self.filter(
+            unrestricted_condition() | unrestricted_condition("parent__"),
+            **kwargs,
+        )
+
     def visible(self, user, **kwargs):
         """
         Documents are effectively invisible when they have no approved content,
         and the given user is not a superuser, nor allowed to delete documents or
         review revisions, nor a creator of one of the (yet unapproved) revisions.
         """
-        prefix = f"{self.document_relation}__" if self.document_relation else ""
+        prefix = self.document_relation_prefix
 
         locale = kwargs.get(f"{prefix}locale")
 
-        qs = self.filter(**kwargs)
+        qs = self.unrestricted(user, **kwargs)
 
         if not user.is_authenticated:
             # Anonymous users only see documents with approved content.
