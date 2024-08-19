a1b78e089c528bb1a7ee822305dc1e9534cdcde1
Smith Ellis <sellis@mozilla.com>
2024-08-19 16:34:28 -0400

    Add regex that matches any phone number

--- kitsune/sumo/utils.py
+++ kitsune/sumo/utils.py
@@ -320,18 +320,20 @@ def check_for_spam_content(data):
     Currently checks for:
     - Toll free numbers
     - Vanity toll free numbers
+    - Any phone number - gotta catch em all
     - Links in the text.
     """
 
     # keep only the digits in text
     digits = "".join(filter(type(data).isdigit, data))
-    is_toll_free = settings.TOLL_FREE_REGEX.match(digits)
 
+    is_toll_free = settings.TOLL_FREE_REGEX.match(digits)
     is_nanp_number = any(settings.NANP_REGEX.findall(data))
+    is_phone_number = any(settings.ANY_PHONE_NUMBER.findall(data))
 
     has_links = has_blocked_link(data)
 
-    return is_toll_free or is_nanp_number or has_links
+    return is_toll_free or is_nanp_number or is_phone_number or has_links
 
 
 @lru_cache(maxsize=settings.WEBPACK_LRU_CACHE)
